e996570f6e4b826e3759429fd8c4b7890766aaf9
steve wagner <ciroque@live.com>
2022-11-30 21:50:44 -0800

    Solves Day 1

--- lib/day1/solution.ex
+++ lib/day1/solution.ex
@@ -2,16 +2,40 @@ defmodule Day1.Solution do
   @moduledoc """
   Examples:
 
-      iex> {:ok, _} = Day1.Solution.run()
+      iex> {:ok, %{answer: 64929}} = Day1.Solution.part_one()
 
+      iex> {:ok, %{answer: 193697}} = Day1.Solution.part_two()
 
   """
 
-  def run do
-    data = Support.FilerReader.read_file("day1.dat")
+  def part_one do
+    data = Support.FilerReader.read_into_lines("day1.dat")
+    elves_calories = sorted_elves_calories(data)
+    most_calories = Enum.at(elves_calories, 0, :none)
 
-    IO.puts("\n>>>>>> #{__MODULE__}:: data(#{data})")
+    {:ok, %{answer: most_calories}}
+  end
 
-    {:not_implemented, %{}}
+  def part_two do
+    data = Support.FilerReader.read_into_lines("day1.dat")
+    elves_calories = sorted_elves_calories(data)
+    top_three_calories = elves_calories
+    |> Enum.take(3)
+    |> Enum.sum()
+
+    {:ok, %{answer: top_three_calories}}
+  end
+
+  defp sorted_elves_calories(data) do
+    data
+    |> Stream.chunk_by(&(&1 == ""))
+    |> Stream.reject(&(&1 == [""]))
+    |> Stream.map(&sum_list/1)
+    |> Enum.sort(:desc)
+  end
+
+  defp sum_list(list) do
+    integers = Enum.map(list, &String.to_integer/1)
+    Enum.sum(integers)
   end
 end
--- lib/support/filer_reader.ex
+++ lib/support/filer_reader.ex
@@ -3,7 +3,18 @@ defmodule Support.FilerReader do
 
   @spec read_file(String.t()) :: String.t()
   def read_file(filename) do
-    path = Path.join(:code.priv_dir(:adventofcode2022), filename)
+    path = get_private_filename(filename)
     File.read!(path)
+  end
+
+  def read_into_lines(filename) do
+    path = get_private_filename(filename)
+    File.stream!(path)
+    |> Stream.map(&String.trim/1)
+    |> Enum.to_list()
+  end
+
+  defp get_private_filename(filename) do
+    Path.join(:code.priv_dir(:adventofcode2022), filename)
   end
 end
