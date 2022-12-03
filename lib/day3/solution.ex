1f189edf05fe2d6db6168d6a3416aadee1cb57c8
steve wagner <ciroque@live.com>
2022-12-03 12:32:23 -0800

    Day 3, Part 2

--- lib/day3/solution.ex
+++ lib/day3/solution.ex
@@ -22,9 +22,15 @@ defmodule Day3.Solution do
   def part_two do
     data = Support.FileReader.read_into_lines("day3.dat")
 
-    IO.puts("\n>>>>>> #{__MODULE__}:: data(#{inspect(data)})")
+    result = data
+    |> group_into_threes()
+    |> find_common_items_across_groups()
+    |> calculate_priorities()
+    |> Enum.sum()
 
-    {:not_implemented, %{}}
+    {:ok, %{answer: result}}
+
+    {:not_implemented, %{answer: result}}
   end
 
   defp calculate_priorities(items) do
@@ -32,7 +38,26 @@ defmodule Day3.Solution do
     Enum.map(items, &(Map.get(priority_map, &1, 0)))
   end
 
+  defp find_common_items_across_groups(groups), do: Enum.map(groups, &find_common_items_in_group/1)
+
   defp find_common_items_across_rucksacks(rucksacks), do: Enum.map(rucksacks, &find_common_items_in_rucksack/1)
+
+  defp find_common_items_in_group([rucksack_one, rucksack_two, rucksack_three] = _group) do
+
+    rucksack_one_list = split_string(rucksack_one)
+    rucksack_two_list = split_string(rucksack_two)
+    rucksack_three_list = split_string(rucksack_three)
+
+    rucksack_one_ms = MapSet.new(rucksack_one_list)
+    rucksack_two_ms = MapSet.new(rucksack_two_list)
+    rucksack_three_ms = MapSet.new(rucksack_three_list)
+
+    rucksack_one_two_intersection = MapSet.intersection(rucksack_one_ms, rucksack_two_ms)
+    final_intersection = MapSet.intersection(rucksack_one_two_intersection, rucksack_three_ms)
+    [common_item|_] = MapSet.to_list(final_intersection)
+
+    common_item
+  end
 
   defp find_common_items_in_rucksack({compartment_one, compartment_two} = _rucksack) do
     compartment_one_list = split_string(compartment_one)
@@ -43,6 +68,8 @@ defmodule Day3.Solution do
 
     priority
   end
+
+  defp group_into_threes(data), do: Enum.chunk_every(data, 3)
 
   defp load_into_rucksack(data), do: Enum.map(data, &split_into_compartments/1)
 
