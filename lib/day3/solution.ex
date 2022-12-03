048ccfcad377b7ba7bf6cdfc8fd548bebe889fc8
steve wagner <ciroque@live.com>
2022-12-03 11:17:10 -0800

    Day 3, Part One

--- lib/day3/solution.ex
+++ lib/day3/solution.ex
@@ -2,25 +2,57 @@ defmodule Day3.Solution do
   @moduledoc """
   Examples:
 
-      iex> {:ok, %{answer: nil}} = Day3.Solution.part_one()
+      iex> {:ok, %{answer: 8349}} = Day3.Solution.part_one()
 
       iex> {:ok, %{answer: nil}} = Day3.Solution.part_two()
 
   """
 
   def part_one do
-    data = Support.FileReader.read_file("day3.dat")
+    rucksacks = Support.FileReader.read_into_lines("day3.dat")
+    result = rucksacks
+    |> load_into_rucksack()
+    |> find_common_items_across_rucksacks()
+    |> calculate_priorities()
+    |> Enum.sum()
 
-    IO.puts("\n>>>>>> #{__MODULE__}:: data(#{data})")
-
-    {:not_implemented, %{}}
+    {:ok, %{answer: result}}
   end
 
   def part_two do
-    data = Support.FileReader.read_file("day3.dat")
+    data = Support.FileReader.read_into_lines("day3.dat")
 
-    IO.puts("\n>>>>>> #{__MODULE__}:: data(#{data})")
+    IO.puts("\n>>>>>> #{__MODULE__}:: data(#{inspect(data)})")
 
     {:not_implemented, %{}}
   end
+
+  defp calculate_priorities(items) do
+    priority_map = priority_map()
+    Enum.map(items, &(Map.get(priority_map, &1, 0)))
+  end
+
+  defp find_common_items_across_rucksacks(rucksacks), do: Enum.map(rucksacks, &find_common_items_in_rucksack/1)
+
+  defp find_common_items_in_rucksack({compartment_one, compartment_two} = _rucksack) do
+    compartment_one_list = split_string(compartment_one)
+    compartment_two_list = split_string(compartment_two)
+    intersection = MapSet.intersection(MapSet.new(compartment_one_list), MapSet.new(compartment_two_list))
+
+    [priority | _] = MapSet.to_list(intersection)
+
+    priority
+  end
+
+  defp load_into_rucksack(data), do: Enum.map(data, &split_into_compartments/1)
+
+  def priority_map do
+    split_string("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    |> Enum.with_index(1)
+    |> Enum.into(%{})
+  end
+
+  defp split_into_compartments(data), do: String.split_at(data, div(String.length(data), 2))
+
+  defp split_string(string), do: String.split(string, "", trim: true)
 end
